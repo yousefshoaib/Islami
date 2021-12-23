7cab4ddc3e7924858f4aae2205e94ec4a6b9dff9
yousefshoaib <92883458+yousefshoaib@users.noreply.github.com>
2021-12-23 06:32:25 -0800

    finished home screen

---
+++ lib/home/HomeScreen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:islami/hades/hades.dart';
+import 'package:islami/main.dart';
+import 'package:islami/quran/quran.dart';
+import 'package:islami/rideo/rideo.dart';
+import 'package:islami/sebha/sebha.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = 'home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentindex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset('assets/images/main_background.png'),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text('islame'),
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              canvasColor: MyThemeData.primaryColor,
+            ),
+            child: BottomNavigationBar(
+                currentIndex: currentindex,
+                onTap: (index) {
+                  currentindex = index;
+                  setState(() {});
+                },
+                items: [
+                  BottomNavigationBarItem(
+                      icon: ImageIcon(AssetImage('assets/images/ic_quran.png')),
+                      label: ('quran')),
+                  BottomNavigationBarItem(
+                      icon: ImageIcon(AssetImage('assets/images/ic_hades.png')),
+                      label: ('hades')),
+                  BottomNavigationBarItem(
+                      icon: ImageIcon(AssetImage('assets/images/ic_sebha.png')),
+                      label: ('sebha')),
+                  BottomNavigationBarItem(
+                      icon: ImageIcon(AssetImage('assets/images/ic_radio.png')),
+                      label: ('radio')),
+                ]),
+          ),
+          body: Container(
+            child: views[currentindex],
+          ),
+        ),
+      ],
+    );
+  }
+
+  List<Widget> views = [QuranTap(), HadesTap(), SebhaTap(), RideoTap()];
+}
